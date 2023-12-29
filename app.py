15e56ee013ef63169a58269f37974de09fbe145d
Adarsh A Inamdar <adarshinamdar2003@gmail.com>
2023-12-29 14:58:09 +0000

    error debug 2.0

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-from diabetes import load_diabetes_model, predict_diabetes
+from module.diabetes import load_diabetes_model, predict_diabetes
 
 app = Flask(__name__)
 
